2594db2b5c0835c75e5c2c1391fcf3695a503b15
s-fuku <s-fuku@kuru.jp>
2020-08-18 22:29:29 +0900

    main.tsのプラグイン関連を整理

--- app/src/plugins/my-components.ts
+++ app/src/plugins/my-components.ts
@@ -1,19 +1,17 @@
 /**
  * Myコンポーネントをグローバルコンポーネントとして登録するプラグイン
  */
-import { VueConstructor } from "vue/types/vue";
+import Vue from "vue";
 
-export default function install(Vue: VueConstructor): void {
-  // Myコンポーネントフォルダ配下の拡張子vue、tsファイルを取得
-  const requireContext = require.context(
-    "@/components/my",
-    true,
-    /[A-Z]\w+\.(vue|ts)$/
-  );
+// Myコンポーネントフォルダ配下の拡張子vue、tsファイルを取得
+const requireContext = require.context(
+  "@/components/my",
+  true,
+  /[A-Z]\w+\.(vue|ts)$/
+);
 
-  // コンポーネントをグローバル登録する
-  requireContext.keys().forEach((fileName) => {
-    const component = requireContext(fileName).default;
-    Vue.component(component.name, component);
-  });
-}
+// コンポーネントをグローバル登録する
+requireContext.keys().forEach((fileName) => {
+  const component = requireContext(fileName).default;
+  Vue.component(component.name, component);
+});
