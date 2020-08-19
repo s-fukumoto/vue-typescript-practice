fc014ec5e79e584572dea07131a8ca02ce241865
s-fuku <s-fuku@kuru.jp>
2020-08-19 19:15:02 +0900

    ファイル名のハッシングを無効化

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+
+  // ソースマップ
   productionSourceMap: process.env.NODE_ENV === "production" ? false : true,
+
+  // ファイル名のハッシング
+  filenameHashing: false,
+
   // productionをGitHubPages用に以下変更
   publicPath:
     process.env.NODE_ENV === "production" ? "/vue-typescript-practice" : "/",
